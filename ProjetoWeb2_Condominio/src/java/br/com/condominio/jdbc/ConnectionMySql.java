3798e967c8f137297ecbd5727b1daace2eaaa2a5
Lourivaldo <Lourivaldo@Lourivaldo-PC>
2014-11-08 15:34:42 -0300

    Conexão com banco de dados remoto

---
+++ ProjetoWeb2_Condominio/src/java/br/com/condominio/jdbc/ConnectionMySql.java
@@ -0,0 +1,47 @@
+package br.com.condominio.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.servlet.ServletContext;
+
+/**
+ *
+ * @author Lourivaldo
+ */
+public class ConnectionMySql {
+
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://25.138.13.112:3306/web2_condominio";
+        String username = "root";
+        String password = "ljfcv08011994";
+        Connection connection = null;
+
+        try {
+            System.out.println("Loading driver...");
+            Class.forName("com.mysql.jdbc.Driver");
+            System.out.println("Driver loaded!");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Cannot find the driver in the classpath!", e);
+        }
+
+        try {
+            System.out.println("Connecting database...");
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Database connected!");
+        } catch (SQLException e) {
+            throw new RuntimeException("Cannot connect the database!", e);
+        } 
+//        finally {
+//            System.out.println("Closing the connection.");
+//            if (connection != null) {
+//                try {
+//                    connection.close();
+//                } catch (SQLException ignore) {
+//                }
+//            }
+//        }
+
+        return connection;
+    }
+}
